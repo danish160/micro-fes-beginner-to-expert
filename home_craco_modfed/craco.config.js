b8246813cecd6a1ae054f19d77da716bbc2355f4
Danish Sultan <danish.sultan@pral.com.pk>
2022-02-21 23:15:22 +0500

    added craco

---
+++ home_craco_modfed/craco.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  output: {
+    publicPath: "http://localhost:3020/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3020,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
