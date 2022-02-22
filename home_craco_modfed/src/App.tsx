894468ea796f46d5f4c0c82f2953bd6b64a720ad
Danish Sultan <danish.sultan@pral.com.pk>
2022-02-22 20:47:31 +0500

    added sass and tailwind

--- home_craco_modfed/src/App.tsx
+++ home_craco_modfed/src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+      <div className="flex">
+        <div className="flex-grow flex">
+          <a>Fidget Spinner World</a>
+          <div className="mx-5">|</div>
+          <a id="cart">Cart</a>
+        </div>
+        <div className="flex-end relative"></div>
+      </div>
     </div>
   );
 }
